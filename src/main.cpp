8ced82d9bc6a6da74f422984b3c4bcf9e59f7c38
FilipHanzel <filip.j.hanzel@gmail.com>
2024-05-20 01:33:42 +0200

    replace debug print/println with printf

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #define DEBUG true
 
 #if DEBUG
-    #define PRINT(...) Serial.print(__VA_ARGS__)
-    #define PRINTLN(...) Serial.println(__VA_ARGS__)
+#define PRINTF(...) Serial.printf(__VA_ARGS__)
 #else
-    #define PRINT(...)
-    #define PRINTLN(...)
+#define PRINTF(...)
 #endif
 
 TaskHandle_t controlerTaskHandle;
